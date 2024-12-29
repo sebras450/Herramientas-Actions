2177c96918d9d469c702c59325ca8941c9bf89dd
Sebastian <sebastian@kali-sebras.sebras.com>
2024-12-28 18:12:21 -0600

    2024-12-28 18:12:21 V1

---
+++ monitoring-sources/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Datos del servidor de inicio
+
+size_filesystem=500
+max_length=$((size_filesystem/10))
+name_filesystem=/Minio
+
+# Datos obtenidos en el trancurso del día
+DATA_FILE="date.txt"
---
+++ monitoring-sources/push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Solicitud de info
+echo "Introduce el archivo o todos [.]"
+read action_commit
+echo "Introduce el texto del commit"
+read text_commit
+
+git add $action_commit
+
+git commit -m "$(date +"%Y-%m-%d %H:%M:%S") V$text_commit"
